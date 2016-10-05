229d88dbc7bb0a14296f15bc215a8c348e022204
hemolyticus <meljdelbarrio@yahoo.com>
2016-10-05 13:56:27 +1300

    Added VideoView

--- MainActivity.java
+++ MainActivity.java
@@ -2,6 +2,7 @@ package ru.mjdelbarrio.videodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView video = (VideoView) findViewById(R.id.videoViewSample);
+        video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.SampleVideo_1280x720_1mb);
+        video.start();
     }
 }
